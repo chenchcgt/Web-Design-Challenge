6a5ecf454b83b5b4795539d91056a23dedb41cf0
Hung Chen <chen_chc@hotmail.com>
2020-08-06 22:44:09 -0400

    final commit

--- readcsv.py
+++ readcsv.py
@@ -5,7 +5,8 @@ import pandas as pd
 csvFile = pd.read_csv("Resources/cities.csv") 
   
 data = csvFile
-
+# reset index
+data.set_index('City_ID', inplace=True)
 # convert to html and save as a file
 
 data.to_html(open('cities_file.html', 'w'))
